6808a7a7b962a8627428d7475fca5cb58308e78e
WixMak <88415615+WixMak@users.noreply.github.com>
2022-06-03 13:58:09 +0800

    Update project.py

--- project.py
+++ project.py
@@ -1,7 +1,11 @@
+# package
 import requests
 import time
-
+# send request while raspberry pi is on
 while True:
+  # send light check request
   requests.post('https://maker.ifttt.com/trigger/ambient_light_detect/json/with/key/{APIKEY}')
+  # send distance check request
   requests.post('https://maker.ifttt.com/trigger/distance_check/json/with/key/{APIKEY}')
+  # timeout
   time.sleep(0.01)
